6be4acb10e6e61da56af8534b849d5786712670b
leo-linux <es.leonardorocha@gmail.com>
2025-04-24 21:54:58 -0300

    feat: add delete loading state to task item

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,4 +1,6 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
+import { toast } from "sonner";
 
 import {
   CheckIcon,
@@ -8,7 +10,25 @@ import {
 } from "../assets/icons/index";
 import Button from "./Button";
 
-const TaskItem = ({ task, handleTaskCheckboxClick, handleDeleteClick }) => {
+const TaskItem = ({ task, handleTaskCheckboxClick, onDeleteSuccess }) => {
+  const [deleteIsLoading, setDeleteIsLoading] = useState(false);
+
+  const handleDeleteClick = async () => {
+    setDeleteIsLoading(true);
+    const response = await fetch(`http://localhost:3000/tasks/${task.id}`, {
+      method: "DELETE",
+    });
+
+    if (!response.ok) {
+      setDeleteIsLoading(false);
+      return toast.error(
+        "Erro ao deletar a tarefa. Por Favor, tente novamente."
+      );
+    }
+    onDeleteSuccess(task.id);
+    setDeleteIsLoading(false);
+  };
+
   const getStatusClasses = () => {
     if (task.status === "done") {
       return "bg-brand-primary text-brand-primary";
@@ -47,8 +67,16 @@ const TaskItem = ({ task, handleTaskCheckboxClick, handleDeleteClick }) => {
         {task.title}
       </div>
       <div className="flex items-center gap-2">
-        <Button color="ghost" onClick={() => handleDeleteClick(task.id)}>
-          <TrashIcon className="text-brand-text-gray" />
+        <Button
+          color="ghost"
+          onClick={handleDeleteClick}
+          disabled={deleteIsLoading}
+        >
+          {deleteIsLoading ? (
+            <LoaderIcon className="animate-spin text-brand-text-gray" />
+          ) : (
+            <TrashIcon className="text-brand-text-gray" />
+          )}
         </Button>
         <a>
           <DetailsIcon />
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -46,17 +46,7 @@ const Tasks = () => {
     setAddTaskDialogIsOpen(false);
   };
 
-  const handleTaskDeleteClick = async (taskId) => {
-    const response = await fetch(`http://localhost:3000/tasks/${taskId}`, {
-      method: "DELETE",
-    });
-
-    if (!response.ok) {
-      return toast.error(
-        "Erro ao deletar a tarefa. Por Favor, tente novamente."
-      );
-    }
-
+  const onDeleteTaskSuccess = async (taskId) => {
     const newTasks = tasks.filter((task) => task.id != taskId);
     setTasks(newTasks);
     toast.success("Tarefa removida com sucesso");
@@ -137,7 +127,7 @@ const Tasks = () => {
               key={task.id}
               task={task}
               handleTaskCheckboxClick={handleTaskCheckboxClick}
-              handleDeleteClick={handleTaskDeleteClick}
+              onDeleteSuccess={onDeleteTaskSuccess}
             />
           ))}
         </div>
@@ -149,7 +139,7 @@ const Tasks = () => {
               key={task.id}
               task={task}
               handleTaskCheckboxClick={handleTaskCheckboxClick}
-              handleDeleteClick={handleTaskDeleteClick}
+              onDeleteSuccess={onDeleteTaskSuccess}
             />
           ))}
         </div>
@@ -161,7 +151,7 @@ const Tasks = () => {
               key={task.id}
               task={task}
               handleTaskCheckboxClick={handleTaskCheckboxClick}
-              handleDeleteClick={handleTaskDeleteClick}
+              onDeleteSuccess={onDeleteTaskSuccess}
             />
           ))}
         </div>
