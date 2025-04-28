2c3b2b684a5677020f029afe0a900faaacae0729
Leo Rocha <ep.leonardorocha@gmail.com>
2025-04-28 20:04:56 -0300

    feat: ensure task can be edited

--- src/pages/task-details.jsx
+++ src/pages/task-details.jsx
@@ -1,10 +1,15 @@
-import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { toast } from "sonner";
 
-import { ArrowLeftIcon, ChevronRightIcon, TrashIcon } from "../assets/icons";
+import {
+  ArrowLeftIcon,
+  ChevronRightIcon,
+  LoaderIcon,
+  TrashIcon,
+} from "../assets/icons";
 import Button from "../components/Button";
 import Input from "../components/Input";
-import InputLabel from "../components/InputLabel";
 import Sidebar from "../components/Sidebar";
 import TimeSelect from "../components/TimeSelect";
 
@@ -12,6 +17,12 @@ const TaskDetailsPage = () => {
   const { taskId } = useParams();
   const [task, setTask] = useState();
   const navigate = useNavigate();
+  const [saveIsLoading, setSaveIsLoading] = useState(false);
+  const [errors, setErrors] = useState([]);
+
+  const titleRef = useRef();
+  const descriptionRef = useRef();
+  const timeRef = useRef();
 
   const handleBackClick = () => {
     navigate(-1);
@@ -29,6 +40,65 @@ const TaskDetailsPage = () => {
     fecthTask();
   }, [taskId]);
 
+  const handleSaveClick = async () => {
+    setSaveIsLoading(true);
+    const newErrors = [];
+    const title = titleRef.current.value;
+    const description = descriptionRef.current.value;
+    const time = timeRef.current.value;
+
+    if (!task.title.trim()) {
+      newErrors.push({
+        inputName: "title",
+        message: "O titulo é obrigatório.",
+      });
+    }
+
+    if (!task.time.trim()) {
+      newErrors.push({
+        inputName: "time",
+        message: "O horário é obrigatório.",
+      });
+    }
+
+    if (!task.description.trim()) {
+      newErrors.push({
+        inputName: "description",
+        message: "A descrição é obrigatória.",
+      });
+    }
+
+    setErrors(newErrors);
+
+    if (newErrors.length > 0) {
+      return setSaveIsLoading(false);
+    }
+
+    setSaveIsLoading(true);
+    const response = await fetch(`http://localhost:3000/tasks/${task.id}`, {
+      method: "PATCH",
+      body: JSON.stringify({
+        title,
+        description,
+        time,
+      }),
+    });
+
+    if (!response.ok) {
+      return setSaveIsLoading(false);
+    }
+    const newTask = await response.json();
+    setTask(newTask);
+    toast.success("Tarefa salva com sucesso!.");
+    setSaveIsLoading(false);
+  };
+
+  const titleError = errors.find((error) => error.inputName === "title");
+  const timeError = errors.find((error) => error.inputName === "time");
+  const descriptionError = errors.find(
+    (error) => error.inputName === "description"
+  );
+
   return (
     <div className="flex">
       <Sidebar />
@@ -44,12 +114,9 @@ const TaskDetailsPage = () => {
               <ArrowLeftIcon />
             </button>
             <div className="flex items-center gap-1 text-xs">
-              <span
-                onClick={handleBackClick}
-                className="cursor-pointer text-brand-text-gray"
-              >
+              <Link to={"/"} className="text-brand-text-gray">
                 Minhas Tarefas
-              </span>
+              </Link>
               <ChevronRightIcon className="text-brand-text-gray" />
               <span className="font-semibold text-brand-primary">
                 {task?.title}
@@ -67,27 +134,42 @@ const TaskDetailsPage = () => {
         {/* Dados da Tarefa */}
         <div className="mt-6 space-y-6 rounded-xl bg-brand-white p-6">
           <div>
-            <Input id="title" label="Título" value={task?.title} />
+            <Input
+              error={titleError}
+              id="title"
+              label="Título"
+              defaultValue={task?.title}
+              ref={titleRef}
+            />
           </div>
 
           <div>
-            <TimeSelect value={task?.time} />
+            <TimeSelect
+              error={timeError}
+              defaultValue={task?.time}
+              ref={timeRef}
+            />
           </div>
 
           <div>
             <Input
+              error={descriptionError}
               id="description"
               label="Descrição"
-              value={task?.description}
+              defaultValue={task?.description}
+              ref={descriptionRef}
             />
           </div>
         </div>
 
         <div className="flex w-full justify-end gap-3">
-          <Button size="large" color="secondary">
-            Cancelar
-          </Button>
-          <Button size="large" color="primary">
+          <Button
+            disabled={saveIsLoading}
+            onClick={handleSaveClick}
+            size="large"
+            color="primary"
+          >
+            {saveIsLoading && <LoaderIcon className="mr-2 animate-spin" />}
             Salvar
           </Button>
         </div>
